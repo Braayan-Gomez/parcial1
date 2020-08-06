4eed1b08973c700efb3d44c74e4990da0096c579
Braayan-Gomez <bra.gomez@udla.edu.com>
2020-08-06 16:50:31 -0500

    parcial 1

---
+++ js/index.js
@@ -0,0 +1,74 @@
+
+		function init() {
+
+	var num7 = document.getElementById("nun7");
+	var num8 = document.getElementById("nun8");
+	var num9 = document.getElementById("nun9");
+	var num4 = document.getElementById("nun4");
+	var num5 = document.getElementById("nun5");
+	var num6 = document.getElementById("nun6");
+	var num1 = document.getElementById("nun1");
+	var num2 = document.getElementById("nun2");
+	var num3 = document.getElementById("nun3");
+	var ce = document.getElementById("ce");
+	var num0 = document.getElementById("num0");
+	var igual = document.getElementById("igual");
+	var sum = document.getElementById("sum");
+	var res = document.getElementById("res");
+	var mul = document.getElementById("mul");
+	var divi = document.getElementById("divi");
+	var codigo = document.getElementById("codigo");
+	var resultado = dodocument.getElementById("resultado");
+
+
+
+
+
+	num1.onclick = function (e) {
+		resultado.textContent = resultado.textContent + "1";
+	}
+	num2.onclick = function (e) {
+		resultado.textContent=resultado.Textcontent + "2"
+	}
+	num3.onclick = function (e) {
+		resultado.textcontent=resultado.Textcontent + "3"
+	}
+	num4.onclick = function (e) {
+		resultado.textcontent=resultado.Textcontent + "4"
+	}
+	num5.onclick = function (e) {
+		resultado.textcontent=resultado.Textcontent + "5"
+	}
+	num6.onclick = function (e) {
+		resultado.textcontent=resultado.Textcontent + "6"
+	}
+	num7.onclick = function (e) {
+		resultado.textcontent=resultado.Textcontent + "7"
+	}
+	num8.onclick = function (e) {
+		resultado.textcontent=resultado.Textcontent + "8"
+	}
+	num9.onclick = function (e) {
+		resultado.textcontent=resultado.Textcontent + "9"
+	}
+
+	num0.onclick = function (e) {
+		resultado.textcontent=resultado.Textcontent + "0"
+	}
+	ce.onclick = function (e) {
+		resetear()
+	}
+	ce.onclick = function (e) {
+		resetear()
+	}
+
+
+	
+}
+
+	
+	//
+
+
+	
+
